ee0ce9e63087fc94bf6ba54620f4d4b39e6ccedf
areg1 <areg.ohanyan.99@mail.ru>
2020-02-28 01:18:38 -0800

    initial setup

---
+++ application/views/main_view.php
@@ -0,0 +1,4 @@
+<?php
+print_r($data);
+?>
+<h1>All ok</h1>
---
+++ application/views/template_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Главная</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<?php include 'application/views/'.$content_view; ?>
+</body>
+</html>
